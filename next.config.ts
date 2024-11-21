edbd133baa93b219887f92d8ca8d59e0130b692e
vikasazad <vikumar.azad@gmail.com>
2024-11-21 14:44:08 +0530

    21/11/2024

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   reactStrictMode: true,
   compress: true,
   images: {
