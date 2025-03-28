a1162620450f469493fed67eb61b85792d9661c0
vikasazad <vikumar.azad@gmail.com>
2025-03-28 16:30:05 +0530

    28/03/2025

--- src/app/modules/order/components/OrderCard.tsx
+++ src/app/modules/order/components/OrderCard.tsx
@@ -208,7 +208,7 @@ export default function OrderCard() {
           if (table?.tag === "restaurant") {
             removeTableByNumber(table?.email, table?.tableNo);
           }
-          router.push("/orderConfirmation");
+          router.prefetch("/orderConfirmation");
           sendNotification(
             token,
             "New Order Received",
